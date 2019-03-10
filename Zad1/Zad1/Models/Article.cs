9b4358aee6512defdc8012b565512d1248c85e1e
jurczewski <bartek.jurczewski@gmail.com>
2019-03-10 14:06:20 +0100

    Added: Data files, sgm parser, sgm nuget, model Articel and example of running program

---
+++ Zad1/Zad1/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Zad1.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public Dictionary<string, List<string>> Tags { get; set; }
+        public List<string> Words { get; set; }
+    }
+}
--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Zad1.Models;
 
 namespace Zad1
 {
-    class Program
-    {
+    public class Program
+    {        
+        List<string> _validPlaces = new List<string>
+        {
+            "west-germany",
+            "usa",
+            "france",
+            "uk",
+            "canada",
+            "japan"
+        };
         static void Main(string[] args)
         {
+            List<Article> _readDocuments = new DocumentReader().ObtainVectorSpaceModels().ToList();
+            //foreach(var article in _readDocuments)
+            //{
+            //    Console.WriteLine("Title: " + article.Title);
+            //}
+
+            Console.WriteLine("******\n" + _readDocuments[0].Title);
+
+            foreach (var word in _readDocuments[0].Words)
+            {
+                Console.WriteLine(word);
+            }
+
+            foreach (var tag in _readDocuments[0].Tags)
+            {
+                Console.WriteLine(tag.Key + ", Number of Values: " + tag.Value.Count);
+            }
+
+            Console.ReadKey();
         }
     }
 }
