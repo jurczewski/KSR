d4253a815f03b73048bfb3174d9043f84bc97b02
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-04-02 16:29:53 +0200

    Manhattan, Euclidean and IMetric added.

---
+++ Zad1/Zad1/Metrics/EuclideanMetric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.Metrics
+{
+    public class EuclideanMetric : IMetric
+    {
+        public double GetDistance(List<double> first, List<double> second)
+        {
+            double distance = 0;
+            for (int i = 0; i < first.Count; i++)
+            {
+                distance += Math.Pow(first[i] - second[i], 2);
+            }
+
+            return Math.Sqrt(distance);
+        }
+    }
+}
---
+++ Zad1/Zad1/Metrics/IMetric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.Metrics
+{
+    public interface IMetric
+    {
+        double GetDistance(List<double> first, List<double> second);
+    }
+}
