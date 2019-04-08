c0e998dd8f1057828d0498488e162d87e6f83cbb
Bartosz Jurczewski <24575781+jurczewski@users.noreply.github.com>
2019-04-08 15:27:43 +0200

    New function Run with whole program

--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Zad1.Metrics;
@@ -8,24 +8,10 @@ namespace Zad1
 {
     public class Program
     {
-
-        static void Main(string[] args)
+        private static void Run(List<string> validTerms, string CategoryName, double trainingToTestDataRatio, int stopListWordNumber, IMetric metric, int k, bool whichAlgorithm)
         {
-            List<string> validPlaces = new List<string>
-            {
-                "west-germany",
-                "usa",
-                "france",
-                "uk",
-                "canada",
-                "japan"
-            };
-
-            //List<string> usaCities = new List<string> {"new york", "los angeles", "chicago", "houston", "phoenix", "philadelphia"};
-            double trainingToTestDataRatio = 0.6;
-            int stopListWordNumber = 100;
             List<Article> allArticles = new DocumentReader().ObtainVectorSpaceModels().ToList();
-            List<Article> validArticles = ArticleUtils.GetArticlesWithValidTags(allArticles, "places", validPlaces);
+            List<Article> validArticles = ArticleUtils.GetArticlesWithValidTags(allArticles, CategoryName, validTerms);
             int trainingArticleNumber = Convert.ToInt32(trainingToTestDataRatio * validArticles.Count);
             List<Article> trainingArticles = validArticles.Take(trainingArticleNumber).ToList();
             List<Article> testArticles = validArticles.Skip(trainingArticleNumber).ToList();
@@ -48,7 +34,7 @@ namespace Zad1
             }
 
             List<string> stopList = new List<string>();
-            foreach(var w in allWords.OrderByDescending(v => v.Value).Take(stopListWordNumber))
+            foreach (var w in allWords.OrderByDescending(v => v.Value).Take(stopListWordNumber))
             {
                 stopList.Add(w.Key);
                 //Console.Write(w.Key + " ");
@@ -57,21 +43,28 @@ namespace Zad1
 
             List<ProcessedArticle> processedTrainingArticles = new List<ProcessedArticle>();
 
-            foreach(Article a in trainingArticles)
+            foreach (Article a in trainingArticles)
             {
                 processedTrainingArticles.Add(ArticleUtils.processArticle(a, stopList));
             }
 
             List<string> keyWords = new List<string>();
-            foreach(var p in validPlaces)
+            foreach (var p in validTerms)
             {
                 List<ProcessedArticle> articles = processedTrainingArticles.Where(a => a.label == p).ToList();
                 List<string> words = new List<string>();
-                foreach(var a in articles)
+                foreach (var a in articles)
                 {
                     words.AddRange(a.StemmedWords);
                 }
-                keyWords.AddRange(ArticleUtils.getMostFrequentWords(ArticleUtils.tf(words), 20));
+                if (whichAlgorithm)
+                {
+                    //keyWords.AddRange(ArticleUtils.getMostFrequentWords(ArticleUtils.idf(words), 20));
+                }
+                else
+                {
+                    keyWords.AddRange(ArticleUtils.getMostFrequentWords(ArticleUtils.tf(words), 20));
+                }
             }
 
             processedTrainingArticles.ForEach(a => a.createFeatureVector(keyWords));
@@ -84,15 +77,57 @@ namespace Zad1
             }
 
             processedTestArticles.ForEach(a => a.createFeatureVector(keyWords));
-            
-            foreach(var a in processedTestArticles)
-            {
-                string guessed = Knn.guesLabel(new EuclideanMetric(), 3, processedTrainingArticles, a);
 
-                string log = "Guessed: " + guessed + " should be: " + a.label;
-                Console.WriteLine(log);
+            int correctNumber = 0;
+            foreach (var a in processedTestArticles)
+            {
+                string guessed = Knn.guesLabel(metric, k, processedTrainingArticles, a);
+
+                //string log = $"Guessed: { guessed} should be: { a.label}";
+                if (guessed == a.label)
+                {
+                    correctNumber++;
+                }
             }
 
+            Console.WriteLine($"For k = {k}, category: {CategoryName}, correct are: {((float)correctNumber / (float)processedTestArticles.Count()) * 100}");
+        }
+
+        private static readonly string CategoryNamePlaces = "places";
+        private static readonly string CategoryNameTopics = "topics";
+
+        static void Main(string[] args)
+        {
+            List<string> validPlaces = new List<string>
+            {
+                "west-germany",
+                "usa",
+                "france",
+                "uk",
+                "canada",
+                "japan"
+            };
+
+            List<string> validTopics = new List<string>
+            {
+                "gold",
+                "cocoa",
+                "sugar",
+                "coffe",
+                "grain"
+            };
+
+            double trainingToTestDataRatio = 0.6;
+            int stopListWordNumber = 100;
+
+            int[] ks = { 2, 3, 5, 7, 10, 15, 20 };
+
+            for (var i = 0; i < ks.Count(); i++)
+            {
+                Program.Run(validPlaces, CategoryNamePlaces, trainingToTestDataRatio, stopListWordNumber, new EuclideanMetric(), ks[i], false);
+                Program.Run(validPlaces, CategoryNamePlaces, trainingToTestDataRatio, stopListWordNumber, new ChebyshevMetric(), ks[i], false);
+                Program.Run(validPlaces, CategoryNamePlaces, trainingToTestDataRatio, stopListWordNumber, new ManhattanMetric(), ks[i], false);
+            }
 
             Console.ReadKey();
         }
