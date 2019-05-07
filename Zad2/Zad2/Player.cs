45010ed403ffd2112d9f99b57b330b092e4f17a7
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-05-07 21:35:35 +0200

    [ADDED] Model class for Player

---
+++ Zad2/Zad2/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace Zad2
+{
+    public class Player
+    {
+        public int Id { set; get; }
+        public int Age { set; get; }
+        public string Value { set; get; }
+        public string Wage { set; get; }
+        public string Height { set; get; }
+        public string Weight { set; get; }
+        public string Fk_accuracy { set; get; }
+        public string Sprint_speed { set; get; }
+        public string Stamina { set; get; }
+        public string Strength { set; get; }
+    }
+}
